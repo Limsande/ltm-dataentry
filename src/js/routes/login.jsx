dc5ddcce0e8efba524528b5f360cac3078a0e879
Luis Imsande <limsande@yahoo.com>
2021-05-30 17:24:33 +0200

    Add login and register dummy code

--- src/js/routes/login.jsx
+++ src/js/routes/login.jsx
@@ -1,7 +1,73 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default class Login extends React.Component {
-    render () {
-        return <h1>Login</h1>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            loginValid: true,
+            loggedIn: false
+        }
+    }
+
+    updateEmail = (ev) => {
+        this.setState({
+            email: ev.target.value
+        });
+    }
+
+    handleLogin = (ev) => {
+        ev.preventDefault();
+        if (this.state.email === 'test@example.com') {
+            this.setState({
+                loginValid: true,
+                loggedIn: true
+            });
+        } else {
+            this.setState({
+                loginValid: false
+            });
+        }
+    }
+
+    /**
+     * Render the component
+     */
+    render() {
+        if (this.state.loggedIn){
+            return <h2>Already logged in</h2>
+        }
+
+        let errorTag = null;
+        let errorLabel = null;
+        let errorInput = null;
+        if (!this.state.loginValid) {
+            errorTag = (
+                <span className="form-error is-visible">No user exists with the e-mail address {this.state.email}.</span>
+            );
+            errorLabel = 'is-invalid-label';
+            errorInput = 'is-invalid-input';
+        }
+        return (
+            <main className="grid-container-x small callout">
+                <h1>Login</h1>
+                <form onSubmit={this.handleLogin}>
+                    <div>
+                        <label className={errorLabel}>E-Mail Address
+                            <input type="email" name="email" placeholder="Your e-mail address" tabIndex="1" required="required" value={this.state.email} onChange={this.updateEmail} className={errorInput} />
+                            {errorTag}
+                        </label>
+                    </div>
+                    <div className="text-right">
+                        <button tabIndex="2" className="button">Login</button>
+                    </div>
+                </form>
+                <div>
+                    <p>You don't have an account yet? <Link to="/register">Create an account</Link></p>
+                </div>
+            </main>
+        )
     }
 }
--- src/js/routes/register.jsx
+++ src/js/routes/register.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default class Register extends React.Component {
-    render () {
-        return <h1>Register</h1>
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            role: '',
+            emailInUse: false,
+            success: false
+        }
+    }
+
+    updateEmail = (ev) => {
+        this.setState({
+            email: ev.target.value
+        });
+    }
+
+    updateRole = (ev) => {
+        this.setState({
+            role: ev.target.value
+        });
+    }
+
+    handleRegister = (ev) => {
+        ev.preventDefault();
+        if (this.state.email === 'test@example.com') {
+            this.setState({
+                emailInUse: true
+            });
+        } else {
+            this.setState({
+                success: true
+            });
+        }
+    }
+
+    /**
+     * Render the component
+     */
+    render() {
+        if (this.state.success){
+            return <h2>Account created</h2>
+        }
+
+        let errorTag = null;
+        let errorLabel = null;
+        let errorInput = null;
+        if (this.state.emailInUse) {
+            errorTag = (
+                <span className="form-error is-visible">An account already exists with the e-mail address {this.state.email}.</span>
+            );
+            errorLabel = 'is-invalid-label';
+            errorInput = 'is-invalid-input';
+        }
+        return (
+            <main className="grid-container-x small callout">
+                <h1>Create an account</h1>
+                <form onSubmit={this.handleRegister}>
+                    <div>
+                        <label className={errorLabel}>E-Mail Address
+                            <input type="email" name="email" placeholder="Your e-mail address" tabIndex="1" required="required" value={this.state.email} onChange={this.updateEmail} className={errorInput} />
+                            {errorTag}
+                        </label>
+                        <label>Role
+                            <select value={this.state.role} onChange={this.updateRole}>
+                                <option value="admin">Admin</option>
+                                <option value="contributor">Contributor</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div className="text-right">
+                        <button tabIndex="2" className="button">Create account</button>
+                    </div>
+                </form>
+                <div>
+                    <p>Already have an account? <Link to="/login">Login</Link></p>
+                </div>
+            </main>
+        )
     }
 }

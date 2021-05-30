ab84cbc60c6d8c3eaa2a0f5aa079cb1c25826f66
Luis Imsande <limsande@yahoo.com>
2021-05-30 16:06:59 +0200

    Connect data view page to API

--- src/js/routes/data/index.jsx
+++ src/js/routes/data/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link} from "react-router-dom";
 import {readEndpoint} from "redux-json-api";
 
 class Index extends React.Component {
@@ -16,12 +17,41 @@ class Index extends React.Component {
      * Render the component
      */
     render() {
-        return <h1>List of data</h1>
+        let content;
+        if(this.props.data && this.props.data.length > 0) {
+            content = this.props.data.data_items.map((data_item) => {
+                let key = 'entry-' + data_item.id;
+                let path = '/data/' + data_item.id;
+                return (
+                    <div>
+                        <Link to={path}>{data_item.attributes.idGlobal}</Link>
+                        <Link to={path}>{data_item.attributes.surnameGiven}</Link>
+                    </div>
+                )
+            });
+        } else {
+            content = <h1>Nothing to see here</h1>;
+        }
+        return (
+            <div>
+                {content}
+            </div>
+        )
     }
 }
 
 function mapStateToProps(state) {
-    return null
+    if (state.api && state.api.data) {
+        return {
+            data: state.api.data
+        }
+    } else {
+        return {
+            data: {
+                data_items: []
+            }
+        };
+    }
 }
 
 export default connect(mapStateToProps)(Index);
--- src/js/routes/data/view.jsx
+++ src/js/routes/data/view.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { readEndpoint } from 'redux-json-api';
 import {Link} from "react-router-dom";
+import {FormattedMessage} from "react-intl";
 
+/**
+ * Displays a table with all (attribute, value) pairs of given data item.
+ */
 class View extends React.Component {
 
+    /**
+     * @param props
+     *  - location.loggedIn Whether the user is logged in
+     */
     constructor(props) {
         super(props);
+
+        this.state = {
+            loggedIn: this.props.location.loggedIn
+        };
     }
 
     componentDidMount() {
+        // fetch data item
         this.props.dispatch(readEndpoint('data/' + this.props.match.params.mid))
     }
 
@@ -17,26 +30,85 @@ class View extends React.Component {
      * Render the component
      */
     render() {
+        if (!this.props.data) {
+            // data not yet retrieved
+            return <div><FormattedMessage id="view.message.loading"/></div>;
+        }
 
-        const loginURL = {
-            pathname: "/login",
-            redirectTo: document.location.pathname
-        };
+        let id = this.props.match.params.mid
+        if (!this.props.data[id]){
+            // no data item with this ID
+            return <div><FormattedMessage id="view.message.notFound" /></div>
+        }
 
+        let item = this.props.data[this.props.match.params.mid];
+        let editPath = '/data/' + item.id + '/edit';
+
+        /*
+         * Display buttons to edit this data item, if user is logged in. Display link to
+         * login page, otherwise.
+         */
+        let editButtons;
+        if (!this.state.loggedIn) {
+            const loginURL = {
+                pathname: "/login",
+                redirectTo: document.location.pathname
+            };
+
+            editButtons = <Link to={loginURL}><FormattedMessage id="view.button.loginToEdit" /></Link>;
+        } else {
+            editButtons = (
+                <div>
+                    <Link to={editPath} className="button">
+                        <FormattedMessage id="view.button.proposeChange" />
+                    </Link>
+                    <Link to={editPath} className="button">
+                        <FormattedMessage id="view.button.proposeChange" />
+                    </Link>
+                </div>
+            );
+        }
+
+        /*
+         * Render data as table of (attribute, value) pairs.
+         */
         return (
             <div>
-                <h1>Values of this data</h1>
-
-                {/* link to login
-                * see (https://reacttraining.com/react-router/web/api/location) */}
-                <Link to={loginURL}>Login to edit</Link>
+                <main className="grid-container">
+                    <div className="grid-x grid-padding-x">
+                        {editButtons}
+                        <div className="cell">
+                            <h1>{item.attributes.surnameGiven}, {item.attributes.firstnameGiven}</h1>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th><FormattedMessage id="view.tableHeading.attribute" /></th>
+                                        <th><FormattedMessage id="view.tableHeading.value" /></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {item.attributes.map((attr, i) => <tr key={i}><td>attr</td><td>item.attributes.attr</td></tr>)}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </main>
             </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    return null
+    let props = {
+        data: null
+    }
+    if (state.api && state.api.data) {
+        props.data = {};
+        state.api.data.data_items.forEach((data_item) => {
+            props.data[data_item.id] = data_item;
+        });
+    }
+    return props;
 }
 
 export default connect(mapStateToProps)(View);
